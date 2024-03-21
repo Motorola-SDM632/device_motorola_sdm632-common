6e8978f112163cb2c70177a0c4ac1c3245e724f3
Demon000 <demon000@github.com>
2024-03-21 15:02:00 +0000

    sdm632-common: do not build useless packages

--- common.mk
+++ common.mk
@@ -103,9 +103,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
-    camera.device@3.2-impl:32 \
     libbson.vendor \
-    libxml2
 
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
@@ -220,9 +218,7 @@ PRODUCT_COPY_FILES += \
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
-    IPACM_cfg.xml \
-    libipanat \
-    liboffloadhal
+    IPACM_cfg.xml
 
 # IRQ
 PRODUCT_COPY_FILES += \
@@ -483,7 +479,6 @@ PRODUCT_PACKAGES += \
     libqsap_sdk \
     libwifi-hal-qcom \
     libwpa_client \
-    wificond \
     WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
